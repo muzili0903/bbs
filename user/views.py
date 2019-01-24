7e89f091cf9dee13643c0690d9f56029c3200c39
muzili <2484167983@qq.com>
2019-01-24 20:39:27 +0800

    完成了注册功能

--- user/views.py
+++ user/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-
+# from hashlib import md5  #md5可以被彩虹表攻击
+from django.contrib.auth.hashers import make_password, check_password
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from user.forms import RegisterFrom
+
+
 def register(request):
+    if request.method == 'POST':
+        # 取出数据
+        form = RegisterFrom(request.POST, request.FILES)
+        if form.is_valid():
+            # 创建一个user,commit参数：暂时不提交到数据库
+            user = form.save(commit=False)
+            # 保存图片
+            # 密码加密处理
+            user.password = make_password(user.password)
+            # 保存用户
+            user.save()
+            # 登录跳转
+            request.session['uid'] = user.id
+            request.session['uid'] = user.nickname
+            return redirect('/user/login/')
+        else:
+            return render(request, '', {'error': form.errors})
     return render(request, '', {})
 
 
